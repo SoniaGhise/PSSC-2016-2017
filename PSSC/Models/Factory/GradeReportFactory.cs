c126a8c4fa2e04e75c5929b17d97220881121d18
SoniaGhise <ghise_sonia@yahoo.com>
2016-11-06 21:24:45 +0200

    Factories

---
+++ PSSC/Models/Factory/GradeReportFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Factory
+{
+    public class GradeReportFactory
+    {
+        
+        public static readonly GradeReportFactory Instance = new GradeReportFactory();
+
+        public GradeReportFactory()
+        { 
+        
+        }
+
+        public Student.GradeReport createInstance(Student.GradeReport gradeReport)
+        {
+            return gradeReport;
+        }
+    
+    }
+}
---
+++ PSSC/Models/Factory/SubjectsFactory.cs
@@ -0,0 +1,33 @@
+﻿using Models.Generics;
+using Models.Generics.Exceptions;
+using System;
+using Models.Subject;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace Models.Factory
+{
+    public class SubjectsFactory
+    {
+        public static readonly SubjectsFactory Instance = new SubjectsFactory();
+        List<Subject.Subject> _subjects;
+
+    
+        private SubjectsFactory()
+        {
+
+        }
+          
+        public Subject.Subject createInstance(Subject.Subject subject)
+        {
+            
+            subject = new Subject.Subject(new Subject.SubjectInformation());
+
+            return subject;
+        }
+
+        
+    }
+}
